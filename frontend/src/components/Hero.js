57307e19e25c28179c82d8e58135523e08439627
Bob's Programming Academy <97769721+BobsProgrammingAcademy@users.noreply.github.com>
2024-09-04 18:58:11 +0200

    Code refactoring

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import AOS from 'aos';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import Box from '@mui/material/Box';
