987141f09a2af8da41929d5b51b7ea6740a97090
Bob's Programming Academy <97769721+BobsProgrammingAcademy@users.noreply.github.com>
2024-09-07 01:36:36 +0200

    Code refactoring

--- frontend/src/layout/Sidebar.js
+++ frontend/src/layout/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
